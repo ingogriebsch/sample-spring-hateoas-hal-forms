dbd8f8aeb8f3d6c4d33fe611dcc4437ab48533c9
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-09-22 16:32:38 +0200

    feat(project): add root resource

---
+++ src/main/java/com/github/ingogriebsch/sample/spring/hateoas/hal/forms/root/RootController.java
@@ -0,0 +1,79 @@
+/*-
+ * #%L
+ * Spring HATEOAS HAL-FORMS sample
+ * %%
+ * Copyright (C) 2018 - 2019 Ingo Griebsch
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.ingogriebsch.sample.spring.hateoas.hal.forms.root;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.springframework.hateoas.UriTemplate.of;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.web.util.UriComponentsBuilder.fromUri;
+
+import java.util.List;
+
+import com.github.ingogriebsch.sample.spring.hateoas.hal.forms.inbox.InboxController;
+
+import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class RootController {
+
+    static final String REL_INBOXES = "inboxes";
+    static final String PATH_ROOT = "/api";
+
+    @NonNull
+    private final HateoasPageableHandlerMethodArgumentResolver pageableResolver;
+
+    @GetMapping(PATH_ROOT)
+    public ResponseEntity<RepresentationModel<?>> root() {
+        return ok(new RepresentationModel<>(links()));
+    }
+
+    private List<Link> links() {
+        return newArrayList(selfLink(), inboxesLink());
+    }
+
+    private Link inboxesLink() {
+        Link inboxesLink = linkTo(methodOn(InboxController.class).findAll(null)).withRel(REL_INBOXES);
+
+        UriComponentsBuilder builder = fromUri(inboxesLink.getTemplate().expand());
+        TemplateVariables templateVariables = pageableResolver.getPaginationTemplateVariables(null, builder.build());
+
+        UriTemplate template = of(inboxesLink.getHref()).with(templateVariables);
+        return new Link(template.toString(), REL_INBOXES);
+    }
+
+    private static Link selfLink() {
+        return linkTo(methodOn(RootController.class).root()).withSelfRel();
+    }
+
+}
---
+++ src/test/java/com/github/ingogriebsch/sample/spring/hateoas/hal/forms/root/RootControllerTest.java
@@ -0,0 +1,72 @@
+/*-
+ * #%L
+ * Spring HATEOAS HAL-FORMS sample
+ * %%
+ * Copyright (C) 2018 - 2019 Ingo Griebsch
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.ingogriebsch.sample.spring.hateoas.hal.forms.root;
+
+import static com.github.ingogriebsch.sample.spring.hateoas.hal.forms.root.RootController.PATH_ROOT;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.springframework.hateoas.MediaTypes.HAL_FORMS_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.github.ingogriebsch.sample.spring.hateoas.hal.forms.HateoasConfiguration;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@ExtendWith(SpringExtension.class)
+@Import({ HateoasConfiguration.class })
+@WebMvcTest(RootController.class)
+public class RootControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Nested
+    class Root {
+
+        @Test
+        public void should_return_ok_including_self_link() throws Exception {
+            ResultActions actions = mockMvc.perform(get(PATH_ROOT).accept(HAL_FORMS_JSON));
+
+            actions.andExpect(status().isOk());
+            actions.andExpect(content().contentType(HAL_FORMS_JSON));
+            actions.andExpect(jsonPath("$._links.self.href", endsWith(PATH_ROOT)));
+
+        }
+
+        @Test
+        public void should_return_ok_including_self_inboxLink() throws Exception {
+            ResultActions actions = mockMvc.perform(get(PATH_ROOT).accept(HAL_FORMS_JSON));
+
+            actions.andExpect(status().isOk());
+            actions.andExpect(content().contentType(HAL_FORMS_JSON));
+            actions.andExpect(jsonPath("$._links.inboxes.href", endsWith("/api/inboxes{?page,size,sort}")));
+        }
+    }
+}
